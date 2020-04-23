78ae3f852db61e5da649f48106735caa59405e09
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2020-04-23 15:09:52 +0530

    sample commit

--- src/main/java/ControlStruc.java
+++ src/main/java/ControlStruc.java
@@ -35,7 +35,6 @@ public class ControlStruc extends javax.swing.JFrame {
     
     public ControlStruc(String para) {
         initComponents();
-        
         jTextArea1.setText(para);
     }
 
