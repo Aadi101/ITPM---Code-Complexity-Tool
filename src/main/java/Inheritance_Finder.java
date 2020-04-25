52edc7099170462b0eaea56edb07700a2a9ff47c
GayaniPKarunaratne <gayanikarunaratne222@gmail.com>
2020-04-26 00:39:22 +0530

    correction

--- src/main/java/Inheritance_Finder.java
+++ src/main/java/Inheritance_Finder.java
@@ -5,12 +5,7 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
-import javax.xml.ws.spi.http.HttpContext;
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 /**
  *
