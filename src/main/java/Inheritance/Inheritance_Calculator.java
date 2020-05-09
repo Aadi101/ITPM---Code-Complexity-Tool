0d2c97024aa6945e2061084ef641854223ff0c90
visnaOshani <jayasinghe548@gmail.com>
2020-05-09 10:16:26 +0530

    completed calculator class for inheritance

--- src/main/java/Inheritance/Inheritance_Calculator.java
+++ src/main/java/Inheritance/Inheritance_Calculator.java
@@ -5,7 +5,7 @@
  */
 package Inheritance;
 
-import java.util.ArrayList;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 /**
@@ -17,23 +17,67 @@ public class Inheritance_Calculator {
     private static Inheritance_Calculator uniqueInstance;
 	private static final String inheritedClass = "(public class)\\s([a-zA-Z]+)\\s(extends)\\s([a-zA-Z]+)";
 	private static final String normalClass = "(public class)\\s([a-zA-Z]+)";
-	
-        int weightValue;
-	Pattern pattern;
+	ArrayList<String> programStatements;
+	int weightValue;
+	   Pattern pattern;
 	String [] statementParts;
 	ArrayList<String> baseClassToBe;
 	int i =0;
-        
-        public static Inheritance_Calculator getInstance(ArrayList<String> programStatements) {
+	
+	
+	public static Inheritance_Calculator getInstance(ArrayList<String> programStatements) {
 		if(uniqueInstance == null) {
 			uniqueInstance = new Inheritance_Calculator(programStatements);
 		}
 		return uniqueInstance;
 	}
-        
-        public Inheritance_Calculator(ArrayList<String> programStatements) {
-		//this.programStatements = programStatements;
+	
+	
+	public Inheritance_Calculator(ArrayList<String> programStatements) {
+		this.programStatements = programStatements;
 		baseClassToBe = new ArrayList<String> ();
 	}
         
+        public void calCCI(ArrayList<String> programStatements) {
+		for(int count=0;count<programStatements.size();count++) {
+			
+			if(programStatements.get(count).contains("class") && programStatements.get(count).contains("extends")) {
+				pattern = Pattern.compile(inheritedClass);
+				                        Matcher m = pattern.matcher(programStatements.get(count));
+				if (m.find()){
+							
+            			statementParts = programStatements.get(count).split(" ");
+					if(!(baseClassToBe.size() == 0)) {
+						for(int x=0; x<baseClassToBe.size(); x++) {
+							if(baseClassToBe.get(x).equals(statementParts[2])){ 
+								weightValue ++;
+							}
+						}
+					}
+					
+					if (baseClassToBe.size() == 0) {
+						weightValue ++;
+					}
+					
+					baseClassToBe.add(statementParts[4].replaceAll("\\s", ""));		
+				} 
+			}
+			
+			else if (programStatements.get(count).contains("class")) {
+				pattern = Pattern.compile(normalClass);
+				Matcher m = pattern.matcher(programStatements.get(count));
+				if (m.find()){
+					weightValue = 1;
+				}				
+			}
+		}		
+		weightValue++;
+		
+	}
+        
+        public int calculateCI() {
+		weightValue = 0;
+		this.calCCI(this.programStatements);
+		return weightValue;
+	}
 }
