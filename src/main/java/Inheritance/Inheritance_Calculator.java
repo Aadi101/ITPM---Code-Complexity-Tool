c4aa4fa17fb52394b0468ef7175186ef69a7fd00
visnaOshani <jayasinghe548@gmail.com>
2020-05-09 10:11:07 +0530

    implementation of the calculator class

--- src/main/java/Inheritance/Inheritance_Calculator.java
+++ src/main/java/Inheritance/Inheritance_Calculator.java
@@ -5,6 +5,9 @@
  */
 package Inheritance;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  *
  * @author Samanmali
@@ -15,4 +18,22 @@ public class Inheritance_Calculator {
 	private static final String inheritedClass = "(public class)\\s([a-zA-Z]+)\\s(extends)\\s([a-zA-Z]+)";
 	private static final String normalClass = "(public class)\\s([a-zA-Z]+)";
 	
+        int weightValue;
+	Pattern pattern;
+	String [] statementParts;
+	ArrayList<String> baseClassToBe;
+	int i =0;
+        
+        public static Inheritance_Calculator getInstance(ArrayList<String> programStatements) {
+		if(uniqueInstance == null) {
+			uniqueInstance = new Inheritance_Calculator(programStatements);
+		}
+		return uniqueInstance;
+	}
+        
+        public Inheritance_Calculator(ArrayList<String> programStatements) {
+		//this.programStatements = programStatements;
+		baseClassToBe = new ArrayList<String> ();
+	}
+        
 }
